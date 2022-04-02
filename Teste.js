334b9f54bb3c0dd0900bc937cbbf7cc4ee1c88fe
JrAdilson <imjrgonc@gmail.com>
2022-04-02 10:50:34 -0300

    Testes iniciais - For

---
+++ Teste.js
@@ -0,0 +1,20 @@
+// const nome = prompt("Qual o seu nome?")
+// const idade = prompt("Quantos anos você tem?")
+// const ling = prompt("Qual linguagem de programação você está estudando?")
+
+// const msg = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${ling}!`
+// alert(msg)
+// const resposta = prompt(`Você gosta de estudar ${ling}? Responda com o número 1 para SIM ou 2 para NÃO.`)
+// if(resposta == 1){
+//     const resp = 'Muito bom! Continue estudando e você terá muito sucesso.'
+//     alert(resp)
+// }else if(resposta == 2){
+//     const pesr = 'Ahh que pena... Já tentou aprender outras linguagens?'
+//     alert(pesr)
+// }
+for(i=0; i < 2; i++){
+    let areas = prompt("Tem alguma tecnologia que vc deseja seguir?")
+    let saera = areas.split(" ")
+    let count = 0;
+    console.log(saera[i])
+}
